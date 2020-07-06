f79eba7b04abf721243a0ff8786ac8fe66d7d139
BrandenSoropia <b.soropia@gmail.com>
2020-07-06 18:27:04 -0400

    Return all data needed for an island page.

--- controllers/island.go
+++ controllers/island.go
@@ -2,11 +2,12 @@ package controllers
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/BrandenSoropia/dodo-getaways-backend/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -22,45 +23,56 @@ func IslandCollection(c *mongo.Database) {
 
 // GetIslands : Get islands from the database (default 10). Currently no order.
 func GetIslands(w http.ResponseWriter, r *http.Request) {
-	// we created Book array
-	// var islands []models.Island
+	var islands []models.Island
 
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 
 	opts := options.Aggregate().SetMaxTime(2 * time.Second)
-	cur, _ := collection.Aggregate(ctx, mongo.Pipeline{{{"$lookup", bson.D{{"from", "owners"}, {"localField", "owner"}, {"foreignField", "_id"}, {"as", "owner_details"}}}}}, opts)
+	cur, _ := collection.Aggregate(ctx, mongo.Pipeline{
+		{{"$lookup", bson.D{
+			{"from", "owners"},
+			{"localField", "owner_id"},
+			{"foreignField", "_id"},
+			{"as", "owner_details"},
+		}}},
+		{{"$project", bson.D{
+			{"owner_id", 1},
+			{"name", 1},
+			{"hemisphere", 1},
+			{"description", 1},
+			{"is_draft", 1},
+			{"images", 1},
+			{"owner_details", bson.D{
+				{"$arrayElemAt", bson.A{"$owner_details", 0}},
+			}},
+		}}},
+	}, opts)
 
-	// var results []models.Island
-	// if err = cur.All(context.TODO(), &results); err != nil {
-	// 	log.Fatal(err)
-	// }
 	defer cur.Close(ctx)
 
-	// TODO: Figure out how to structure the Owner data from the lookup into an Owner instance...
 	for cur.Next(ctx) {
-		var result bson.M
-		err := cur.Decode(&result)
+		var island models.Island
+		err := cur.Decode(&island)
+
 		if err != nil {
 			log.Fatal(err)
 		}
-		// TODO: Get associated owner
-		fmt.Print(result)
 
-		// islands = append(islands, result)
+		islands = append(islands, island)
 	}
 
-	// if err := cur.Err(); err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-	// w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 
-	// js, err := json.Marshal(islands)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
+	js, err := json.Marshal(islands)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-	// w.Header().Set("Content-Type", "application/json")
-	// w.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
--- models/models.go
+++ models/models.go
@@ -1,22 +1,25 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Island : Struct for island data from DB.
 type Island struct {
-	ID          primitive.ObjectID `json:"_id" bson:"_id"`
-	Owner       *Owner             `json:"owner" bson:"owner_id"`
-	Name        string             `json:"name" bson:"name"`
-	Hemisphere  string             `json:"hemisphere" bson:"hemisphere"`
-	Description string             `json:"description" bson:"description"`
-	IsDraft     bool               `json:"is_draft" bson:"isDraft"`
-	Images      []string           `json:"images" bson:"images"`
+	ID           primitive.ObjectID `json:"_id" bson:"_id"`
+	OwnerID      primitive.ObjectID `json:"owner_id" bson:"owner_id"`
+	Name         string             `json:"name" bson:"name"`
+	Hemisphere   string             `json:"hemisphere" bson:"hemisphere"`
+	Description  string             `json:"description" bson:"description"`
+	IsDraft      bool               `json:"is_draft" bson:"isDraft"`
+	Images       []string           `json:"images" bson:"images"`
+	OwnerDetails Owner              `json:"owner_details" bson:"owner_details,omitempty"`
 }
 
 // Owner : Struct for owner data from DB.
 type Owner struct {
 	ID              primitive.ObjectID `json:"_id" bson:"_id"`
-	Island          *Island            `json:"island" bson:"island_id"`
+	IslandID        primitive.ObjectID `json:"island_id" bson:"island_id"`
 	Username        string             `json:"username" bson:"username"`
 	DiscordUsername string             `json:"discord_username" bson:"discord_username"`
 	Description     string             `json:"description" bson:"description"`
